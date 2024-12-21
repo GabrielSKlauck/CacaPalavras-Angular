5b5da70146df35805edacd44764e413ff0ab47cf
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-12-21 17:50:10 -0300

    Mudanças na estilização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatrizPalavrasComponent } from "./matriz-palavras/matriz-palavras.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatrizPalavrasComponent],
+  imports: [RouterOutlet, MatrizPalavrasComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/matriz-palavras/matriz-palavras.component.ts
+++ src/app/matriz-palavras/matriz-palavras.component.ts
@@ -12,8 +12,11 @@ export class MatrizPalavrasComponent{
   caca = new CacaPalavras();
   
   itens:string[][] = this.caca.matriz;
+  listaPalavras:string[] = ['Banana','Abacate','Morango','Uva']
 
   ngOnInit(): void {
     console.log(this.caca.matriz)
   }
+
+  
 }
